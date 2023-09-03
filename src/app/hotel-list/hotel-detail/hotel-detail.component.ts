534ce6c47c0d48301a3fb931523bc5dcdfccb80a
kevindeffo <deffokevin14@gmail.com>
2023-09-03 04:31:56 +0100

    navigation with routerLink

--- src/app/hotel-list/hotel-detail/hotel-detail.component.ts
+++ src/app/hotel-list/hotel-detail/hotel-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {HotelListService} from "../hotel-list.service";
+import {IHotel} from "../hotel";
 
 @Component({
   selector: 'app-hotel-detail',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HotelDetailComponent implements OnInit {
 
-  constructor() { }
+  public hotel: IHotel | undefined = <IHotel>{};
+
+  constructor(private route: ActivatedRoute, private hotelListService: HotelListService) { }
 
   ngOnInit(): void {
+    // @ts-ignore
+    const id: number = +this.route.snapshot.paramMap.get('id');
+    console.log(id);
+
+    this.hotelListService.getHotels().subscribe((hotels: IHotel[]) => {
+      this.hotel = hotels.find(hotel => hotel.hotelId == id);
+      console.log(this.hotel);
+    })
   }
 
 }
